0c9ecd051df2a97ab9baff0238a8bf657fac5944
Irina Kryshen <irinakryshen77@gmail.com>
2026-09-06 12:33:25 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -2,54 +2,69 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
 import { getImagesByQuery } from "./js/pixabay-api";
-import { clearGallery, createGallery, hideLoader, showLoader } from "./js/render-functions";
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from "./js/render-functions";
+
 const form = document.querySelector(".form");
-form.addEventListener ("submit", hundlerSubmit)
 
-function hundlerSubmit (event) {
-    event.preventDefault();
-    const query = event.currentTarget.elements["search-text"]
-    if (query.value.trim() === "") {
+form.addEventListener("submit", hundlerSubmit);
+
+function hundlerSubmit(event) {
+  event.preventDefault();
+
+  const query = event.currentTarget.elements["search-text"];
+  const searchValue = query.value.trim();
+
+  if (searchValue === "") {
+    iziToast.show({
+      message: "Please enter search word",
+      color: "red",
+      position: "topRight",
+      timeout: 5000,
+    });
+
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(searchValue)
+    .then(data => {
+      const images = Array.isArray(data) ? data : data.hits;
+
+      if (!images || images.length === 0) {
         iziToast.show({
-    message: 'Please enter search word',
-    color: 'red',
-    position: 'topRight',
-    timeout: 5000,
-})
-        return
-    } 
+          message:
+            "Sorry, there are no images matching your search query. Please try again!",
+          color: "red",
+          position: "topRight",
+          timeout: 5000,
+        });
 
-           clearGallery()
-       showLoader()
+        return;
+      }
 
-        const respons = getImagesByQuery (query.value)
-    .then (data => {
-        if (data.length ===0) {
-iziToast.show({
-    message: 'Sorry, there are no images matching your search query. Please try again!',
-    color: 'red',
-    position: 'topRight',
-    timeout: 5000,
-})
-        }
-     else {
-        createGallery(data)
-    }} 
-   )
-        .catch (error => {
-            console.log(error);
-        iziToast.show({
-            message: 'Ops... try again!',
-    color: 'red',
-    position: 'topRight',
-    timeout: 5000,    
-        })    
-        } )
-        .finally (() => {
-            hideLoader()
-                form.reset()
-                }) 
-           
-    }
-    
+      createGallery(images);
+    })
+    .catch(error => {
+      console.log(error);
+
+      iziToast.show({
+        message: "Ops... try again!",
+        color: "red",
+        position: "topRight",
+        timeout: 5000,
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      form.reset();
+    });
+}
